7fe71064cf9b063b0d744fa0f67c457ae20af4b1
Gustavo Ayala <agustavo87@gmail.com>
2021-01-15 17:53:50 -0300

    Feat: Se agregaron los controles de la barra lateral.

--- resources/views/logos/create.blade.php
+++ resources/views/logos/create.blade.php
@@ -11,11 +11,11 @@
   title="Logos"
 >
   <x-container>
-    <form action="/logos" method="POST">
+    {{-- <form action="/logos" method="POST"> --}}
     <div class="flex flex-col mx-auto logos-container mt-3 max-w-screen-lg">
       <input type="text" name="title" id="title" placeholder="Título" class="ml-2 text-3xl font-bold mb-2 focus:outline-none text-gray-800">
       <x-logos  />
     </div>
-    </form>
+    {{-- </form> --}}
   </x-container>
 </x-layout.default>
