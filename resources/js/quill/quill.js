fc77a84fe6c57b418adc867768410e5576b1ef6e
Gustavo Ayala <agustavo87@gmail.com>
2021-01-19 12:11:01 -0300

    Varios cambios

--- resources/js/quill/quill.js
+++ resources/js/quill/quill.js
@@ -2,20 +2,30 @@ import Quill from 'quill/core';
 
 import Toolbar from 'quill/modules/toolbar';
 // import Snow from 'quill/themes/snow';
-import Bubble from 'quill/themes/bubble';
+import Bubble, {BubbleTooltip} from 'quill/themes/bubble';
 
 import Bold from 'quill/formats/bold';
 import Italic from 'quill/formats/italic';
+import Link from 'quill/formats/link';
 import Header from 'quill/formats/header';
 import SourceBlot from 'dsm/quill/blots/source'
 import Citations from 'dsm/quill/modules/Citations'
 import {SourceTypes} from 'dsm/DSM/SourceTypes'
+
+BubbleTooltip.TEMPLATE = [
+  '<span class="ql-tooltip-arrow"></span>',
+  '<div class="ql-tooltip-editor">',
+    '<input type="text" data-formula="e=mc^2" data-link="https://arete.com" data-video="Embed URL">',
+    '<a class="ql-close"></a>',
+  '</div>'
+].join('');
 
 Quill.register({
   'modules/toolbar': Toolbar,
   'themes/bubble': Bubble,
   'formats/bold': Bold,
   'formats/italic': Italic,
+  'formats/link': Link,
   'formats/header': Header
 });
 
--- resources/views/components/logos.blade.php
+++ resources/views/components/logos.blade.php
@@ -181,6 +181,9 @@
       window.scrollTo(0, cursorBottom + margin * 3 - window.innerHeight)
     }
   }
+
+  const videoButton = document.querySelector('#video-button');
+  videoButton.addEventListener('click', () => console.log('insertar video'));
 </script>
 @endpush
 
@@ -205,6 +208,7 @@
         <path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd" />
       </svg>
     </button>
+    <button class="ql-link"></button>
   </span>
 </div>
 
