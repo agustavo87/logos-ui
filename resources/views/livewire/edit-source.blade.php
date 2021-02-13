c2d26cab1c15d14b84aa17a2fd277c524ebe5d29
Gustavo Ayala <agustavo87@gmail.com>
2021-02-13 15:38:06 -0300

    FIX: Se arreglo el asunto de que usar x-if y template impedia el funcionamiento de livewire, se utilizo otro metodo para manejar la tecla escape.

--- resources/views/livewire/edit-source.blade.php
+++ resources/views/livewire/edit-source.blade.php
@@ -1,50 +1,50 @@
-<div class="fixed inset-0 z-10 flex flex-row justify-center items-center"
-    x-data="editSource()" x-show="display"
-    x-on:{{$listen}}.window="handleInvokation"
-    
-    >
+<div class="fixed inset-0 z-10 flex flex-row justify-center items-center" 
+    x-data="editSource()" x-show="display" x-on:{{$listen}}.window="handleInvokation">
 
     {{-- dark background --}}
     <template x-if="withBg">
         <div class="fixed inset-0 bg-gray-700 opacity-40"> </div>
     </template>
 
-    <template x-if="showModal">
-        <div class="rounded-lg bg-white relative w-2/3" 
-            @click.away="cancel"
-            x-on:keydown.escape.window="cancel"  >
-            <div class="py-5 px-5 w-full">
-                <p>Hola</p>
-                <x-form.button @click="solve">Add</x-form.button>
-            </div>
+    <div class="rounded-lg bg-white relative w-2/3" 
+        x-show="showModal" @click.away="cancel" x-on:keydown.escape.window="handleEscape">
+        <div class="py-5 px-5 w-full">
+            <p>Hola</p>
+            <x-form.button @click="solve">Add</x-form.button>
         </div>
-    </template>
+    </div>
 
-<script>
-    function editSource() {
-        return {
-            display: false,
-            showModal: false, 
-            withBg: @json($withBg),
-            resolve: r => console.log(r),
-            handleInvokation: function (e) {
-                console.log('invoked:', e)
-                this.resolve = e.detail.resolve,
-                this.withBg = e.detail.withBg,
-                this.showModal = true;
-                this.display = true;
-            },
-            solve: function () {
-                this.resolve('ok')
-                this.display = false;
-            },
-            cancel: function() {
-                this.display = false;
-                this.showModal = false;
-                console.log('edit-source canceling')
-                this.resolve(null)
+    <script>
+        function editSource() {
+            return {
+                display: false,
+                showModal: false,
+                withBg: @json($withBg),
+                resolve: r => console.log(r),
+                handleInvokation: function(e) {
+                    console.log('invoked:', e)
+                    this.resolve = e.detail.resolve,
+                        this.withBg = e.detail.withBg,
+                        this.showModal = true;
+                    this.display = true;
+                },
+                solve: function() {
+                    this.resolve('ok')
+                    this.display = false;
+                },
+                cancel: function() {
+                    this.display = false;
+                    this.showModal = false;
+                    console.log('edit-source canceling')
+                    this.resolve(null)
+                },
+                handleEscape: function() {
+                    if (this.showModal) {
+                        this.cancel();
+                    }
+                }
+
             }
         }
-    }
-</script>
+    </script>
 </div>
