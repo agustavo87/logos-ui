47c4e123e197f268a06e63e44af29ba93c8a9446
Gustavo Ayala <agustavo87@gmail.com>
2021-05-22 12:37:45 -0300

    FEAT: Configured PHPUnit to look in domain directory.

---
+++ domain/Logos/Tests/ExampleLogosTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleLogosTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_is_invoked()
+    {
+        $this->assertTrue(true);
+    }
+}
