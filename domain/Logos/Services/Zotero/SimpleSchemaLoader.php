f7683d138c70dc8bbe401c1c83d9a16f5591468d
Gustavo Ayala <agustavo87@gmail.com>
2021-05-26 10:28:51 -0300

    FEAT: Added some zotero types

--- domain/Logos/Services/Zotero/SimpleSchemaLoader.php
+++ domain/Logos/Services/Zotero/SimpleSchemaLoader.php
@@ -14,17 +14,130 @@ class SimpleSchemaLoader implements SchemaLoaderInterface
             'version' => 1,
         ]))->addItemTypes([
             [
-                'itemType' => 'journalArticle',
+                'itemType' => 'annotation'
+            ],[
+                'itemType' => 'blogPost',
                 'fields' => [
-                    ['field' => 'title']
+                    ['field' => 'title'],
+                    ['field' => 'abstractNote'],
+                    ['field' => 'blogTitle', 'baseField' => 'publicationTitle'],
+                    ['field' => 'websiteType', 'baseField' => 'type'],
+                    ['field' => 'date'],
+                    ['field' => 'url'],
+                    ['field' => 'accessDate'],
+                    ['field' => 'language'],
+                    ['field' => 'shortTitle'],
+                    ['field' => 'rights'],
+                    ['field' => 'extra'],
                 ],
                 'creatorTypes' => [
-                    [
-                        'creatorType' => 'author',
-                        'primary' => true
-                    ]
+                    ['creatorType' => 'author', 'primary' => true],
+                    ['creatorType' => 'commenter'],
+                    ['creatorType' => 'contributor']
                 ]
-            ]
+            ],[
+                'itemType' => 'book',
+                'fields' => [
+                    ['field' => 'title'],
+                    ['field' => 'abstractNote'],
+                    ['field' => 'series'],
+                    ['field' => 'seriesNumber'],
+                    ['field' => 'volume'],
+                    ['field' => 'numberOfVolumes'],
+                    ['field' => 'edition'],
+                    ['field' => 'place'],
+                    ['field' => 'publisher'],
+                    ['field' => 'date'],
+                    ['field' => 'numPages'],
+                    ['field' => 'language'],
+                    ['field' => 'ISBN'],
+                    ['field' => 'shortTitle'],
+                    ['field' => 'url'],
+                    ['field' => 'accessDate'],
+                    ['field' => 'archive'],
+                    ['field' => 'archiveLocation'],
+                    ['field' => 'libraryCatalog'],
+                    ['field' => 'callNumber'],
+                    ['field' => 'rights'],
+                    ['field' => 'extra'],
+                ],
+                'creatorTypes' => [
+                    ['creatorType' => 'author', 'primary' => true],
+                    ['creatorType' => 'contributor'],
+                    ['creatorType' => 'editor'],
+                    ['creatorType' => 'translator'],
+                    ['creatorType' => 'seriesEditor'],
+                ]
+            ],[
+                'itemType' => 'bookSection',
+                'fields' => [
+                    ['field' => 'title'],
+                    ['field' => 'abstractNote'],
+                    ['field' => 'bookTitle', 'baseField' => 'publicationTitle'],
+                    ['field' => 'series'],
+                    ['field' => 'seriesNumber'],
+                    ['field' => 'volume'],
+                    ['field' => 'numberOfVolumes'],
+                    ['field' => 'edition'],
+                    ['field' => 'place'],
+                    ['field' => 'publisher'],
+                    ['field' => 'date'],
+                    ['field' => 'pages'],
+                    ['field' => 'language'],
+                    ['field' => 'ISBN'],
+                    ['field' => 'shortTitle'],
+                    ['field' => 'url'],
+                    ['field' => 'accessDate'],
+                    ['field' => 'archive'],
+                    ['field' => 'archiveLocation'],
+                    ['field' => 'libraryCatalog'],
+                    ['field' => 'callNumber'],
+                    ['field' => 'rights'],
+                    ['field' => 'extra'],
+                ],
+                'creatorTypes' => [
+                    ['creatorType' => 'author', 'primary' => true],
+                    ['creatorType' => 'contributor'],
+                    ['creatorType' => 'editor'],
+                    ['creatorType' => 'bookAuthor'],
+                    ['creatorType' => 'translator'],
+                    ['creatorType' => 'seriesEditor'],
+                ]
+            ],[
+                'itemType' => 'journalArticle',
+                'fields' => [
+                    ['field' => 'title'],
+                    ['field' => 'abstractNote'],
+                    ['field' => 'publicationTitle'],
+                    ['field' => 'volume'],
+                    ['field' => 'issue'],
+                    ['field' => 'pages'],
+                    ['field' => 'date'],
+                    ['field' => 'series'],
+                    ['field' => 'seriesTitle'],
+                    ['field' => 'seriesText'],
+                    ['field' => 'journalAbbreviation'],
+                    ['field' => 'language'],
+                    ['field' => 'DOI'],
+                    ['field' => 'ISSN'],
+                    ['field' => 'shortTitle'],
+                    ['field' => 'url'],
+                    ['field' => 'accessDate'],
+                    ['field' => 'archive'],
+                    ['field' => 'archiveLocation'],
+                    ['field' => 'libraryCatalog'],
+                    ['field' => 'callNumber'],
+                    ['field' => 'rights'],
+                    ['field' => 'extra'],
+                ],
+                'creatorTypes' => [
+                    ['creatorType' => 'author', 'primary' => true],
+                    ['creatorType' => 'contributor'],
+                    ['creatorType' => 'editor'],
+                    ['creatorType' => 'translator'],
+                    ['creatorType' => 'reviewedAuthor'],
+                ]
+            ],
         ]);
     }
 }
