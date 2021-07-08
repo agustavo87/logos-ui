68a48e2c5b2df2ff34e2f78bb3e96ec8ef579e81
Gustavo Ayala <agustavo87@gmail.com>
2021-07-08 05:47:00 -0300

    FIX: UI Select component

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,11 +1,11 @@
 @props(['name', 'label'])
 <x-form.label :for=$name> {{$label}}: </x-form.label>
-<select 
+<select
   {{ $attributes->merge([
     'name' => $name,
     'id' => $name,
     'aria-label' => $label,
-    'class' => 'p-2 text-red-100leading-tight border border-gray-400 rounded-sm focus:outline-none',
+    'class' => 'p-2 leading-tight border border-gray-400 rounded-sm focus:outline-none',
   ]) }}
 >
   {{ $slot }}
