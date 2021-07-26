2ec385c455ef43637146f9043d14374205a2943d
Gustavo Ayala <agustavo87@gmail.com>
2021-07-26 20:14:37 -0300

    FIX: UI. Alert closes on escape key

--- resources/views/components/modal/alert.blade.php
+++ resources/views/components/modal/alert.blade.php
@@ -5,13 +5,14 @@
     'cancelCaption' => ucfirst(__('ui.cancel'))
     ])
 
-<!-- This example requires Tailwind CSS v2.0+ -->
-<div x-data="modal" class="fixed z-10 inset-0 overflow-y-auto" x-show="modal" @keydown.escape="cancel" x-cloak
+{{-- This example requires Tailwind CSS v2.0+ --}}
+<div x-data="modal" class="fixed z-10 inset-0 overflow-y-auto" x-show="modal" @keyup.escape.window="cancel" x-cloak
     x-on:show-alert.window="externalCall"
 >
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
-      <div class="fixed inset-0 transition-opacity " x-show="modal" aria-hidden="true"
+      {{-- Background overlay --}}
+      <div class="fixed inset-0" x-show="modal" aria-hidden="true"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100"
@@ -25,13 +26,13 @@
       <!-- This element is to trick the browser into centering the modal contents. -->
       <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
-      <div x-show="modal" @click.outside="cancel" class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline"
+      <div x-show="modal"  @click.outside="cancel" class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline"
         x-transition:enter="ease-out duration-300"
-        x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+        x-transition:enter-start="opacity-0 -translate-y-4 "
+        x-transition:enter-end="opacity-100 translate-y-0 "
         x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        x-transition:leave-start="opacity-100 translate-y-0 "
+        x-transition:leave-end="opacity-0 -translate-y-4 "
       >
         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div class="sm:flex sm:items-start">
@@ -54,10 +55,10 @@
           </div>
         </div>
         <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-          <button @click="accept" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+          <button @click="accept" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm">
             {{ $acceptCaption }}
           </button>
-          <button @click="cancel" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+          <button @click="cancel" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
             {{ $cancelCaption }}
           </button>
         </div>
@@ -106,42 +107,5 @@ document.addEventListener('alpine:init', () => {
         }))
     })
 
-    // function modal() {
-    //     return {
-    //         modal: false,
-    //         name: '{{ $name }}',
-    //         acceptCB: () => null, // console.log('aceptado'),
-    //         cancelCB: () => null, //console.log('cancelado'),
-    //         showModal: function (event, acceptCB =  null, cancelCB = null) {
-    //           if (acceptCB) this.acceptCB = acceptCB;
-    //           if (cancelCB) this.cancelCB = cancelCB;
-    //         //   console.log(event);
-    //         //   console.log(this.acceptCB);
-    //         //   console.log(this.cancelCB);
-    //           this.modal = true;
-    //         },
-    //         hideModal: function () {
-    //             this.modal = false;
-    //         },
-    //         accept: function () {
-    //           this.hideModal();
-    //           this.acceptCB();
-    //         },
-    //         cancel: function () {
-    //           this.hideModal();
-    //           this.cancelCB();
-    //         },
-    //         externalCall: function (event) {
-    //             if (!event.detail.name === this.name) return;
-    //             // console.log('me llaman a mi!');
-    //             // console.log(event.detail)
-    //             this.showModal(
-    //                 event,
-    //                 event.detail.accept ? event.detail.accept : null,
-    //                 event.detail.cancel ? event.detail.cancel : null
-    //             );
-    //         }
-    //     }
-    // }
     </script>
   @endpush
