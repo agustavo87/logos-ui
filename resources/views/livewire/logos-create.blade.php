b6e4094097e537ef73448525d2515cf06c6e2279
Gustavo Ayala <agustavo87@gmail.com>
2021-07-26 21:20:46 -0300

    REFACTOR: UI. Logos entangles

--- resources/views/livewire/logos-create.blade.php
+++ resources/views/livewire/logos-create.blade.php
@@ -7,24 +7,18 @@
 </style>
 
 @endpush
-<div
-    x-data="LogosWrapper({
-        deltaEntangle: @entangle('article.delta').defer,
-        metaEntangle: @entangle('article.meta').defer,
-        htmlEntangle: @entangle('article.html').defer
-    })"
->
+<div x-data="LogosWrapper($wire)">
     <div class="max-w-screen-md mx-auto mt-3 h-5">
          {{-- Status information --}}
         <span class=" ml-2 text-xs text-gray-400" x-text="transactionStatus"></span>
     </div>
-    <div    @quill-input="handleQuillInput"
-            class="flex flex-col mx-auto logos-container mt-1 max-w-screen-md"
+    <div x-on:quill-input="handleQuillInput"
+         class="flex flex-col mx-auto logos-container mt-1 max-w-screen-md"
     >
         {{-- Title --}}
-        <input  type="text" wire:model.defer="article.title"
-                @input="handleSimpleInput" placeholder="Título" autocomplete="off"
-                class="ml-2 text-3xl font-bold mb-2 focus:outline-none text-gray-800"
+        <input type="text" placeholder="Título" autocomplete="off"
+               x-model="title" x-on:input="handleSimpleInput"
+               class="ml-2 text-3xl font-bold mb-2 focus:outline-none text-gray-800"
         >
         {{-- Logos Editor --}}
         <x-logos :initial-delta="$article->delta" />
@@ -44,27 +38,33 @@
 @push('head-script')
 <script>
 document.addEventListener('alpine:init', () => {
-    Alpine.data('LogosWrapper', (initData) => ({
-        delta: initData.deltaEntangle,
-        meta: initData.metaEntangle,
-        html: initData.htmlEntangle,
-        transactionStatus: 'Listo',
-        handleQuillInput: function (event) {
-            this.delta = event.detail.delta();
-            this.html = event.detail.html();
-            this.meta = {key: 'value'};
-            this.transactionStatus = 'Modificado'
-            this.save()
-        },
-        handleSimpleInput: function () {
-            this.transactionStatus = 'Modificado'
-            this.save()
-        },
-        save: debounce(function () {
-            this.transactionStatus = "Guardando..."
-            this.$wire.save().then(() => {this.transactionStatus = 'Guardado'});
-        }, 3000, {trailing: true, maxWait: 10000})
-    }))
+    Alpine.data('LogosWrapper', ($wire) => {
+        return {
+            title: $wire.entangle('article.title').defer,
+            delta: $wire.entangle('article.delta').defer,
+            meta: $wire.entangle('article.meta').defer,
+            html: $wire.entangle('article.html').defer,
+            transactionStatus: 'Listo',
+
+            handleQuillInput: function (event) {
+                this.delta = event.detail.delta();
+                this.html = event.detail.html();
+                this.meta = {key: 'value'};
+                this.transactionStatus = 'Modificado'
+                this.save()
+            },
+
+            handleSimpleInput: function () {
+                this.transactionStatus = 'Modificado'
+                this.save()
+            },
+
+            save: debounce(function () {
+                this.transactionStatus = "Guardando..."
+                this.$wire.save().then(() => {this.transactionStatus = 'Guardado'});
+            }, 3000, {trailing: true, maxWait: 10000})
+        }
+    })
 })
 </script>
 @endpush
